ba50aaf92ad3220fdc3b41915d7d195bf5c4ad2f
Daniel Flores <flores.kd97@gmail.com>
2022-04-04 18:42:38 -0700

    made a hover trail

--- script.js
+++ script.js
@@ -9,8 +9,18 @@ function createGrid(numSquares) {
             const square = document.createElement('div');
             square.className = 'box'
             row.appendChild(square)
+            square.addEventListener("mouseover",colorSquare)
         }
 
     }
 }
 createGrid(16);
+
+
+//on click the box keeps the color
+
+
+function colorSquare(event) {
+    event.target.style.backgroundColor = "red"
+}
+
