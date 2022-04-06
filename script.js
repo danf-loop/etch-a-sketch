94a4b8e567b898f254aaf764cca6504ae4c21caf
Daniel Flores <flores.kd97@gmail.com>
2022-04-05 20:55:52 -0700

    implemented to change the size of the grid

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//Creating the grid
+// Creating the grid
 let container = document.querySelector("#container");
 
 const button = document.getElementById("button");
@@ -11,7 +11,7 @@ function createGrid(rows, cols) {
     for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = "box";
-        cell.addEventListener("mouseover", colorSquare)
+        cell.addEventListener("mouseover", colorSquare);
     }
 }
 createGrid(16,16)
@@ -24,7 +24,21 @@ function colorSquare(event) {
 function clearGrid() {
     let container = document.querySelector("#container");
     let squares = container.querySelectorAll("div");
-    squares.forEach((div) => (div.style.backgroundColor = "white"))
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
+
+// prompt to change grid size
+function changeGrid() {
+   let size = prompt("Enter a value between 0 to 100");
+   container.innerHTML = "";
+   if (0 < size && size <= 100) {
+       createGrid(size,size)
+   } else {
+       createGrid(16,16)
+       alert("Only values between 1 - 100")
+   }
+};
+
+
 
 
