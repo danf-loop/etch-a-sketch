0c24d6a803cbe5a3f7d90e63cf37e92a4b9da537
Daniel Flores <flores.kd97@gmail.com>
2022-04-05 20:25:19 -0700

    updated grid to be more dynamic

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 
 //Creating the grid
 let container = document.querySelector("#container");
- 
-function createGrid(numSquares) {
-    for (let i = 0; i < numSquares; i++) {
-        const row = container.appendChild(document.createElement('div'));
-        for (let j = 0; j < numSquares; j++) {
-            const square = document.createElement('div');
-            square.className = 'box'
-            row.appendChild(square)
-            square.addEventListener("mouseover",colorSquare)
-        }
 
+const button = document.getElementById("button");
+
+function clearGrid() {
+    container.style.backgroundColor = "black";
+  }
+ 
+function createGrid(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement('div');
+        container.appendChild(cell).className = "box";
+        cell.addEventListener("mouseover", colorSquare)
     }
 }
-createGrid(16);
 
-
-//on click the box keeps the color
-
-
+createGrid(16,16)
+//hover over the box to draw black boxes
 function colorSquare(event) {
-    event.target.style.backgroundColor = "red"
+    event.target.style.backgroundColor = "black";
 }
 
+// clear the the boxes
+function clearGrid() {
+    let gridSquares = document.querySelectorAll(".box")
+    gridSquares.forEach(box => {box.style.backgroundColor = null});
+}
